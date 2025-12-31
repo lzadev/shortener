3abf232ca757a098d6b4049900b82116680c6f56
Luis Zabala <ldelorden@mardom.com>
2025-12-31 14:40:22 -0400

    feat: add unique index on Code column in ShortUrls table and update ApplicationDbContext

--- Shortener/Shortener.Api/ApplicationDbContext.cs
+++ Shortener/Shortener.Api/ApplicationDbContext.cs
@@ -7,4 +7,17 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
 {
     public DbSet<ShortUrl> ShortUrls { get; set; }
     public DbSet<ShortUrlHistory> ShortUrlHistories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<ShortUrl>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.HasIndex(e => e.Code).IsUnique();
+            entity.Property(e => e.LongUrl).IsRequired();
+            entity.Property(e => e.Code).IsRequired();
+        });
+    }
 }
--- Shortener/Shortener.Api/Migrations/20251231183204_AddIndex.Designer.cs
+++ Shortener/Shortener.Api/Migrations/20251231183204_AddIndex.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Shortener.Api;
@@ -11,9 +12,11 @@ using Shortener.Api;
 namespace Shortener.Api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251231183204_AddIndex")]
+    partial class AddIndex
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
